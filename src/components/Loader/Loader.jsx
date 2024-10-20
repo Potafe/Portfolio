26e74dac9a3640cbd459c7bd0b12587c27649129
Potafe <bt21me011@nitmz.ac.in>
2024-10-20 12:02:50 +0530

    feat: change loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,15 +1,16 @@
 import styles from "./Loader.module.scss";
 
-import { Link } from "../../components";
-
 const Loader = () => {
   return (
     <article className={styles["parent-loader"]}>
-      <div className={styles["loader"]}></div>
-      <p>
-        The loader was taken from{" "}
-        <Link to="https://css-loaders.com/dots/">CSS Loaders</Link>
-      </p>
+      <div className={styles["loader"]}>
+        <span className={styles["letter"]}>Y</span>
+        <span className={styles["letter"]}>A</span>
+        <span className={styles["letter"]}>Z</span>
+        <span className={styles["letter"]}>A</span>
+        <span className={styles["letter"]}>T</span>
+      </div>
+      <p>2025 Yazat Mishra. All rights reserved.</p>
     </article>
   );
 };
